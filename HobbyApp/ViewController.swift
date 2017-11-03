b866b5026afcecf1dc6da4115431042b2073a595
oleizer <oleizer@gmail.com>
2017-11-03 21:56:57 +0600

    Initial Commit

---
+++ HobbyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HobbyApp
+//
+//  Created by Олег Лейзер on 03.11.2017.
+//  Copyright © 2017 Олег Лейзер. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
